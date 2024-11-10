d94275f51f66e3270b8f85b81b7eb637e5953fca
Ibrahim Hosseini <ebrahim.hoseini@sternx.de>
2024-11-10 11:07:40 +0330

    Initial Commit

---
+++ DownloadImageAsync/DownloadImageAsyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadImageAsyncApp.swift
+//  DownloadImageAsync
+//
+//  Created by Ibrahim on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct DownloadImageAsyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
